cd2cd9241b9b4a2b027fc1e1d354321e3ffb816a
Jon Whitcraft <jwhitcraft@mac.com>
2018-05-02 15:44:56 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -54,6 +54,13 @@ resource "aws_security_group" "bastion_host_security_group" {
     to_port     = 22
     cidr_blocks = "${var.cidrs}"
   }
+  
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 443
