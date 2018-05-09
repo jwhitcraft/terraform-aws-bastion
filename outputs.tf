286b7e1caf5020e8bea4dc11053a408aa5e61725
Jon Whitcraft <jwhitcraft@mac.com>
2018-05-09 11:40:54 -0400

    Output the security group id and fix a few things in the user_data.sh

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "bucket_name" {
 output "elb_ip" {
   value = "${aws_lb.bastion_lb.dns_name}"
 }
+
+output "security_group_id" {
+  value = "${aws_security_group.bastion_host_security_group.id}"
+}
